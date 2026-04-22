a160269797198c92369058a4051446d1b11d5d35
yemiiz <415595323@qq.com>
2026-04-22 22:49:52 +0800

    提供功能接口

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,41 @@
 int main()
 {
     staff_manager manager;
-    manager.show_menu();
+
+    int choice = 0;
+    while(true)
+    {
+        manager.show_menu();
+        cout <<"请输入您的选择："<<endl;
+        cin >> choice;
+        switch(choice)
+        {
+        case 0:
+            cout <<"欢迎下次使用！"<<endl;
+            system("pause");
+            exit(0);
+            return 0;
+        case 1:     //添加员工信息
+            break;
+        case 2:     //显示员工信息
+            break;
+        case 3:     //删除员工信息
+            break;
+        case 4:     //修改员工信息
+            break;
+        case 5:    //查找员工信息
+            break;  
+        case 6:        //排序员工信息
+            break;
+        case 7:     //清空员工信息
+            break;
+        default:
+            cout <<"输入有误，请重新输入！"<<endl;
+            break;
+        }   
+        
+            
+
+    }
     return 0;
 }
